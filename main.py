08d016015470a4fc2b00abbaa9b5fbcf281030f0
Ale-delaRosa <ldelarosag1@gmail.com>
2025-03-09 22:26:09 -0600

    version1

---
+++ main.py
@@ -0,0 +1,6 @@
+# Import the training and evaluation function from the neural_networks_keras module
+from src.binary import train_imdb_model
+
+# Check if the script is running directly
+if __name__ == "__main__":  
+   train_imdb_model()  # Call the function to train and evaluate the neural network
